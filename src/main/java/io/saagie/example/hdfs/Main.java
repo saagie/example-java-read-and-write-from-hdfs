5cebb2dfd0df18f681ac4ada7815a408ffe3508c
Pierre Leresteux <pierre.leresteux@gmail.com>
2016-08-23 21:53:45 +0200

    fix(logger) : correct className

--- src/main/java/io/saagie/example/hdfs/Main.java
+++ src/main/java/io/saagie/example/hdfs/Main.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
 
 public class Main {
 
-   private static final Logger logger = Logger.getLogger("io.saagie.example.hdfs.ReadWrite");
+   private static final Logger logger = Logger.getLogger("io.saagie.example.hdfs.Main");
 
    public static void main(String[] args) throws Exception {
       //HDFS URI
