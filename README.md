db82654ce81ce8657344d8b8c613d609e9880618
julien <julien@saagie.com>
2016-09-08 10:27:36 +0200

    use logback instead jul load resources xml (like core-site.xml) from HADOOP_CONF_DIR rewrite pom.xml to optimize package size

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Package for saagie : mvn package and get the package in target.
 Usage in local :
 
  - mvn package
- - java -jar target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs://namenodeserver:8020
+ - java -jar target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT.jar hdfs://cluster
 
 Usage in Saagie :
 
- - mvn package (in local, to generate jar file)
+ - mvn package -Pplatform (in local, to generate jar file)
  - create new Java Job
- - upload the jar (arget/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT-jar-with-dependencies.jar)
+ - upload the jar (target/example-java-read-and-write-from-hdfs-1.0-SNAPSHOT.jar)
  - copy URL from HDFS connection details panel and add it in first argument in the command line
  - choose java 8
  - create and launch.
--- src/main/java/io/saagie/example/hdfs/Main.java
+++ src/main/java/io/saagie/example/hdfs/Main.java
@@ -7,72 +7,78 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.net.URI;
-import java.util.logging.Logger;
 
 public class Main {
 
-   private static final Logger logger = Logger.getLogger("io.saagie.example.hdfs.Main");
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-   public static void main(String[] args) throws Exception {
-      //HDFS URI
 
-      if (args.length<1) {
-         logger.severe("1 arg is required :\n\t- hdfsmasteruri (8020 port) ex: hdfs://namenodeserver:8020");
-         System.err.println("1 arg is required :\n\t- hdfsmasteruri (8020 port) ex: hdfs://namenodeserver:8020");
-         System.exit(128);
-      }
-      String hdfsuri = args[0];
+    public static void main(String[] args) throws Exception {
+        //HDFS URI
 
-      String path="/user/hdfs/example/hdfs/";
-      String fileName="hello.csv";
-      String fileContent="hello;world";
+        if (args.length < 1) {
+            logger.error("1 arg is required :\n\t- hdfsmasteruri (8020 port) ex: hdfs://namenodeserver:8020 or hdfs://clustername");
+            System.err.println("1 arg is required :\n\t- hdfsmasteruri (8020 port) ex: hdfs://namenodeserver:8020 or hdfs://clustername");
+            System.exit(128);
+        }
+        String hdfsuri = args[0];
 
-      // ====== Init HDFS File System Object
-      Configuration conf = new Configuration();
-      // Set FileSystem URI
-      conf.set("fs.defaultFS", hdfsuri);
-      // Because of Maven
-      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-      conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-      // Set HADOOP user
-      System.setProperty("HADOOP_USER_NAME", "hdfs");
-      System.setProperty("hadoop.home.dir", "/");
-      //Get the filesystem - HDFS
-      FileSystem fs = FileSystem.get(URI.create(hdfsuri), conf);
+        String path = "/user/hdfs/example/hdfs/";
+        String fileName = "hello.csv";
+        String fileContent = "hello;world";
 
-      //==== Create folder if not exists
-      Path workingDir=fs.getWorkingDirectory();
-      Path newFolderPath= new Path(path);
-      if(!fs.exists(newFolderPath)) {
-         // Create new Directory
-         fs.mkdirs(newFolderPath);
-         logger.info("Path "+path+" created.");
-      }
 
-      //==== Write file
-      logger.info("Begin Write file into hdfs");
-      //Create a path
-      Path hdfswritepath = new Path(newFolderPath + "/" + fileName);
-      //Init output stream
-      FSDataOutputStream outputStream=fs.create(hdfswritepath);
-      //Cassical output stream usage
-      outputStream.writeBytes(fileContent);
-      outputStream.close();
-      logger.info("End Write file into hdfs");
+        // ====== Init HDFS File System Object
+        Configuration conf = new Configuration();
 
-      //==== Read file
-      logger.info("Read file into hdfs");
-      //Create a path
-      Path hdfsreadpath = new Path(newFolderPath + "/" + fileName);
-      //Init input stream
-      FSDataInputStream inputStream = fs.open(hdfsreadpath);
-      //Classical input stream usage
-      String out= IOUtils.toString(inputStream, "UTF-8");
-      logger.info(out);
-      inputStream.close();
-      fs.close();
+        final String HADOOP_PREFIX = System.getenv("HADOOP_PREFIX");
+        if (HADOOP_PREFIX == null) {
+            final String HADOOP_CONF_DIR = System.getenv("HADOOP_CONF_DIR");
+            logger.info("add resources core-site.xml, hdfs-site.xml, mapred-site.xml");
+            conf.addResource(new Path(HADOOP_CONF_DIR, "core-site.xml"));
+            conf.addResource(new Path(HADOOP_CONF_DIR, "hdfs-site.xml"));
+            conf.addResource(new Path(HADOOP_CONF_DIR, "mapred-site.xml"));
+        }
+        // Set HADOOP user
+        System.setProperty("HADOOP_USER_NAME", "hdfs");
+        //Get the filesystem - HDFS
+        FileSystem fs = FileSystem.get(URI.create(hdfsuri), conf);
 
-   }
+        //==== Create folder if not exists
+        Path newFolderPath = new Path(path);
+        if (!fs.exists(newFolderPath)) {
+            // Create new Directory
+            fs.mkdirs(newFolderPath);
+            logger.info("Path " + path + " created.");
+        }
+
+        //==== Write file
+        logger.info("Begin Write file into hdfs");
+        //Create a path
+        Path hdfswritepath = new Path(newFolderPath + "/" + fileName);
+        //Init output stream
+        FSDataOutputStream outputStream = fs.create(hdfswritepath);
+        //Cassical output stream usage
+        outputStream.writeBytes(fileContent);
+        outputStream.close();
+        logger.info("End Write file into hdfs");
+
+        //==== Read file
+        logger.info("Read file into hdfs");
+        //Create a path
+        Path hdfsreadpath = new Path(newFolderPath + "/" + fileName);
+        //Init input stream
+        FSDataInputStream inputStream = fs.open(hdfsreadpath);
+        //Classical input stream usage
+        String out = IOUtils.toString(inputStream, "UTF-8");
+        logger.info(out);
+        inputStream.close();
+        fs.close();
+
+    }
 }
